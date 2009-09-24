9528a427bb5892d2b1e0f0c596b2cf48eb373fb6
Weston Binford <wbinford@gmail.com>
2009-09-24 06:03:14 +0000

    Created _StarterTree_StructureMap, a project that will use the StructureMap IoC container to configure the application.

---
+++ _StarterTree_StructureMap/src/StarterProject.Tests/HelloWorld_Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using StarterProject.Core;
+
+namespace StarterProject.Tests
+{
+    [TestFixture]
+    public class When_asked_to_speak
+    {
+        [Test]
+        public void should_say_hello_world()
+        {
+            var sut = new HelloWorld();
+            var results = sut.Speak();
+            Assert.AreEqual("Hello, World!", results);            
+        }
+        
+
+    }
+}
