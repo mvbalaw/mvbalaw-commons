8c019bec1d51aee4dc689e4cc4a5c600909c6d51
Weston Binford <wbinford@gmail.com>
2010-02-10 22:08:24 +0000

    First cut at configuring application using FluentNHibernate instead of NHibernate directly.  Also, upgraded FluentNHibernate to 1.0.0.623 in order to get support for NHibernate 2.1.2GA.

--- NHibernateBootstrap-FluentNHibernate/src/NHibernateBootstrap.Core/Persistence/NHibernateRegistry.cs
+++ NHibernateBootstrap-FluentNHibernate/src/NHibernateBootstrap.Core/Persistence/NHibernateRegistry.cs
@@ -1,3 +1,6 @@
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.ByteCode.Castle;
 using NHibernate.Cfg;
@@ -13,13 +16,24 @@ namespace NHibernateBootstrap.Core.Persistence
     {
         public NHibernateRegistry()
         {
-            var cfg = new Configuration()
+            
+             /* var cfg = new Configuration()
                 .SetProperty(Environment.ReleaseConnections, "on_close")
-                .SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName)
+                .SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName) // Fluently.Configure.Database
                 .SetProperty(Environment.ConnectionDriver, typeof(SQLite20Driver).AssemblyQualifiedName)
                 .SetProperty(Environment.ConnectionString, "data source=|DataDirectory|bootstrap.sqlite;Version=3")
                 .SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName)
                 .AddAssembly(typeof(Blog).Assembly);
+              */
+
+            var cfg = Fluently.Configure()
+                .Database(SQLiteConfiguration.Standard.UsingFile("|DataDirectory|bootstrap.sqlite"))
+                .Mappings(m => m.AutoMappings
+                                   .Add(AutoMap.AssemblyOf<Product>()
+                                            .Where(t => t.Namespace == "NHibernateBootstrap.Core.Domain")))
+                .ExposeConfiguration(c => c.SetProperty(Environment.ReleaseConnections, "on_close"))
+                .ExposeConfiguration(c => c.SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName))
+                .BuildConfiguration();
 
             var sessionFactory = cfg.BuildSessionFactory();
 
